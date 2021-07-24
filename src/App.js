ed1c9e8aeb6e771db319d686b3a08cf7d94c9af1
nyralith <walkarel123@hotmail.com.ar>
2021-07-23 22:31:53 -0300

    added react router and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+  
+);
 
 function App() {
   return (
     <div>
-   <HomePage/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
