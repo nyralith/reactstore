15ee3ec918c9eeb9579dab80e0d41f2e19de2461
nyralith <walkarel123@hotmail.com.ar>
2021-07-26 01:29:49 -0300

    added shop component and data, style files added with no content temporarily

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-  
-);
 
 function App() {
   return (
@@ -17,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/shop/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import { react } from '@babel/types';
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/previewcollection/collection-preview.component';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collections: SHOP_DATA
+
+        }
+    }
+
+    render() {
+        const { collections } = this.state;
+        return (<div className='shop-page'>
+            {
+                collections.map(({id, ...OtherCollectionProps}) => (
+                    <CollectionPreview key={id} {...OtherCollectionProps}/>
+                ))
+            }
+        </div>);
+    }
+}
+
+export default ShopPage;
