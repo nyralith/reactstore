fcf8fe3d1c513fb5a60b8db603436dfefd11ccae
nyralith <walkarel123@hotmail.com.ar>
2021-07-28 19:27:19 -0300

    added custom flexible button component+styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -53,7 +54,7 @@ class SignIn extends React.Component {
                         label='Password'
                         required />
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
         )
