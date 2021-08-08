711e20d585621a4e6c1b5fb1571984b7769efc78
nyralith <walkarel123@hotmail.com.ar>
2021-08-07 21:23:23 -0300

    Finished sign in and sign up handleSubmit methods

--- src/App.js
+++ src/App.js
@@ -31,11 +31,9 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-
-          console.log(this.state); 
         });
       }
-      this.setState({currentUser: userAuth});
+      this.setState({ currentUser: userAuth });
     })
 
   }
