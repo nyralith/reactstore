1883807a1185d36778acd5189c698ec909809b09
nyralith <walkarel123@hotmail.com.ar>
2021-08-14 18:50:58 -0300

    added cart component + its styles and icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils'
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -17,18 +18,19 @@ const Header = ({ currentUser }) => (
             </Link>
             <Link className='option' to='/contact'>CONTACT
             </Link>
-        {
-            currentUser ?
-            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>
-        }
+            {currentUser ? (
+                <div className='option' onClick={() => auth.signOut()}>SIGN OUT
+                </div>
+            ) : (
+                <Link className='option' to='/signin'>SIGN IN</Link>
+            )}
+            <CartIcon />
         </div>
     </div>
 )
 
 const mapStateToProps = state => ({
-   currentUser: state.user.currentUser
+    currentUser: state.user.currentUser
 })
 
 export default connect(mapStateToProps)(Header);
